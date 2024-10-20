ccee240176114919976fd4f1834e4fe43e06195b
Grym3038 <156126041+Grym3038@users.noreply.github.com>
2024-10-19 19:35:35 -0500

    Inital/ added context and product class

---
+++ Store_Unit2/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Store_Unit2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a desciption.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter a price.")]
+        public double Price { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a image.")]
+        public string ImagePath { get; set; } 
+
+    }
+}
